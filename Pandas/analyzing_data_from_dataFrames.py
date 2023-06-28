289e981194770c7c22dbf500cb7731398346ad38
Pranav <pranav04092002@gmail.com>
2023-06-28 22:36:03 +0530

    28th June

--- Pandas/analyzing_data_from_dataFrames.py
+++ Pandas/analyzing_data_from_dataFrames.py
@@ -30,3 +30,21 @@ print(total_tests)
 # What fraction of tests returned a positive result
 positive_rate = total_cases / total_tests
 print("The overall positivity rate is {:.2f}%".format(positive_rate * 100))
+
+#!Querying and Sorting Rows
+high_new_cases = covid_df.new_cases > 1000
+print(high_new_cases)
+
+# In this case, it will only show those rows in covid_df whose high_new_cases criteria(new_cases > 1000) is satisfied i.e which are True
+print(covid_df[high_new_cases])
+
+# Can also be done in a single line by:-
+high_cases_df = covid_df[covid_df.new_cases > 1000]
+print(high_cases_df)
+
+"""
+To view all the  rows in the terminal, run:
+from IPython.display import display
+with pd.option_context('display.max_rows', 100):
+    display(covid_df[covid_df.new_cases > 1000])
+"""
